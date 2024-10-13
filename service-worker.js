26bdb660ee23fa86008d9476112a06acbe47a765
nhajt <haoph221@gmail.com>
2024-10-13 15:35:19 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,6 +6,8 @@ self.addEventListener('install', event => {
         '/index.html',
         '/styles.css',
         '/script.js',
+        '/video1.mp4',
+        '/video2.mp4',
         '/icon-192x192.png',
         '/icon-512x512.png'
       ]);
@@ -13,13 +15,6 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
